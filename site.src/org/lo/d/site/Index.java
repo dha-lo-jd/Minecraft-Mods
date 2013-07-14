a178f1f72df3f9532c3876466a4dc9bdfebada4e
dha_lo_jd <dha_1024_blame@hotmail.com>
2013-07-14 22:35:51 +0900

    紹介動画掲載

--- site.src/org/lo/d/site/Index.java
+++ site.src/org/lo/d/site/Index.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Index extends AbstractThymlPage {
 
-	private static final Section[] sections = SimpleSection.newSimpleSections("index", "license", "mods");
+	private static final Section[] sections = SimpleSection.newSimpleSections("index", "license", "movie", "mods");
 
 	public Index() {
 		super("index", sections);
